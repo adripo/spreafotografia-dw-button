ae3694bdaa961b76b427fcc3035433222bd5d172
adripo <26493496+adripo@users.noreply.github.com>
2021-04-25 01:44:08 +0200

    First commit

---
+++ AddDwSpreafotografia.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Add Download full photo button
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @author       adripo
+// @description  Add button to download photo at full resolution on spreafotografia.it
+// @homepage     https://github.com/adripo/spreafotografia-dw-button
+// @icon         http://spreafotografia.it/favicon.ico
+// @updateURL    https://raw.githubusercontent.com/adripo/spreafotografia-dw-button/main/AddDwSpreafotografia.meta.js
+// @downloadURL  https://raw.githubusercontent.com/adripo/spreafotografia-dw-button/main/AddDwSpreafotografia.user.js
+// @supportURL   https://github.com/adripo/yt_signin_popup/wiki
+// @match        http://www.spreafotografia.it/photo-*
+// @grant        none
+// ==/UserScript==
